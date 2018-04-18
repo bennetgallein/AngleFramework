3292a4e99b7b66a735022b0d7bdacfe7ed5b7c24
bennetgallein <bennet@intranetproject.net>
2018-04-18 22:34:30 +0200

    created readme and made it completly work correctly.

---
+++ README.md
@@ -0,0 +1,9 @@
+### Template Engine
+
+Basic Documentation: The first line is the template language and the second is the PHP equivalent
+
+###### echo Variable
+```
+{ :varname }
+<?= $varname; ?>
+```
--- src/TemplateEngine/Syntax.php
+++ src/TemplateEngine/Syntax.php
@@ -14,9 +14,8 @@ class Syntax {
     public $tokens = array();
 
     public function __construct() {
-        $this->addRule('/<!-- :([\w\d]+) -->/', '<?= $$1 ?>');
-        /*$this->addRule("{?:([\w\d]\+)( = .\*)}", "<?php $$1$2; ?>");
-        $this->addRule("{ ?:([\w\d]\+) }", "<?= $$1; ?>");*/
+
+        $this->addRule("/{ :([\w\d]+) }/", "<?= $$1; ?>");
     }
 
     public function addRule($pattern, $replacement) {
